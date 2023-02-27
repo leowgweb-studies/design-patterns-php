9fdb93b4c7445b4e05f58dc731c32267ee6b9f89
Lauro Guedes <leowgweb@gmail.com>
2023-02-27 20:24:57 -0300

    start with adapters pattern

---
+++ src/RegistroOrcamento.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Alura\DesignPattern;
+
+use Alura\DesignPattern\EstadosOrcamento\Finalizado;
+use Alura\DesignPattern\Http\HttpAdapter;
+
+class RegistroOrcamento
+{
+    public function __construct(
+        private HttpAdapter $http
+    ) {
+    }
+
+    public function registrar(Orcamento $orcamento): void
+    {
+        if (!$orcamento->estadoAtual instanceof Finalizado) {
+            throw new \DomainException('Apenas orçamentos finalizados podem ser registrados na API');
+        }
+
+        $this->http->post('http://api.registrar.orcamento', [
+            'valor' => $orcamento->valor,
+            'quantidade' => $orcamento->qtdItens,
+        ]);
+    }
+}
